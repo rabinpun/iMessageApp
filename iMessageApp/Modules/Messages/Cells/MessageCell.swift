3bd961370064c369c86a66ddb4e609ac4ea99595
rabin.pun <rabin.pun@ebpearls.com>
2023-01-09 19:42:01 +0545

    arrange the folder structure

---
+++ iMessageApp/Modules/Messages/Cells/MessageCell.swift
@@ -0,0 +1,12 @@
+//
+//  MessageCell.swift
+//  iMessageApp
+//
+//  Created by ebpearls on 09/01/2023.
+//
+
+import UIKit
+
+final class MessageCell: UICollectionViewCell {
+    
+}
--- iMessageApp/Modules/Messages/MessegesViewController.swift
+++ iMessageApp/Modules/Messages/MessegesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MessegesViewController.swift
 //  iMessageApp
 //
 //  Created by ebpearls on 09/01/2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MessegesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
