dc5db1265e04f5b99fd3fa4838eaf6200fe188ae
rabin.pun <rabin.pun@ebpearls.com>
2023-01-10 08:49:45 +0545

    add identifier in extension for uicollectionviewcell

---
+++ iMessageApp/Helpers/Extensions/UICollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell.swift
+//  iMessageApp
+//
+//  Created by ebpearls on 09/01/2023.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static var identifier:String { String(describing: self) }
+    
+}
