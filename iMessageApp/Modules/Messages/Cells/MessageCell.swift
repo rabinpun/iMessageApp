2584dd471ea02e1cdc803bb8b56e418d9b929a85
rabin.pun <rabin.pun@ebpearls.com>
2023-01-10 09:30:57 +0545

    minor refactor in cell

--- iMessageApp/Modules/Messages/Cells/MessageCell.swift
+++ iMessageApp/Modules/Messages/Cells/MessageCell.swift
@@ -9,24 +9,33 @@ import UIKit
 
 final class MessageCell: UICollectionViewCell {
     
-        var wrapperView: UIView = {
+        lazy var wrapperView: UIView = {
             let view = UIView()
             view.layer.borderWidth = 1.0
             view.layer.borderColor = UIColor.systemGreen.withAlphaComponent(0.5).cgColor
             view.backgroundColor = .systemGreen
             view.clipsToBounds = true
             view.layer.cornerRadius = 10
+            view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
             view.translatesAutoresizingMaskIntoConstraints = false
             return view
         }()
         
-        var label: UILabel = {
+        lazy var label: UILabel = {
             let label = UILabel()
             label.font = .systemFont(ofSize: 14)
             label.textColor = UIColor.white
-            label.textAlignment = .center
+            label.textAlignment = .left
+            label.numberOfLines = 0
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
+        }()
+    
+        lazy var expanderView: UIView = {
+            let view = UIView()
+            view.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
         }()
         
         override init(frame: CGRect) {
@@ -50,24 +59,28 @@ final class MessageCell: UICollectionViewCell {
         func addSubviews() {
             contentView.addSubview(wrapperView)
             wrapperView.addSubview(label)
+            contentView.addSubview(expanderView)
             
             NSLayoutConstraint.activate([
                 wrapperView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-                wrapperView.leadingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                wrapperView.trailingAnchor.constraint(equalTo: expanderView.leadingAnchor),
                 wrapperView.topAnchor.constraint(equalTo: contentView.topAnchor),
                 wrapperView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+    
+                expanderView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                expanderView.topAnchor.constraint(equalTo: contentView.topAnchor),
+                expanderView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
                 
                 label.leadingAnchor.constraint(equalTo: wrapperView.leadingAnchor, constant: 5),
-                label.leadingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -5),
+                label.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -5),
                 label.topAnchor.constraint(equalTo: wrapperView.topAnchor, constant: 5),
                 label.bottomAnchor.constraint(equalTo: wrapperView.bottomAnchor, constant: -5),
             ])
         }
         
-        func configure(title: String, isSelected: Bool, selectedColor: UIColor = .white, unselectedColor: UIColor = .black) {
-            label.text = title
-            label.textColor = isSelected ? selectedColor : unselectedColor
-            wrapperView.backgroundColor = isSelected ? unselectedColor : selectedColor
-        }
+    func configure(title: String) {
+        label.text = title
     }
+    
+}
 
