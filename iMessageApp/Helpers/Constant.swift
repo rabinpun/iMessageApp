bc52e0a76854f7f41234c1ab3954ac4954bcf38a
rabin.pun <rabin.pun@ebpearls.com>
2023-01-10 10:46:15 +0545

    add app constants

---
+++ iMessageApp/Helpers/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  iMessageApp
+//
+//  Created by ebpearls on 10/01/2023.
+//
+
+import Foundation
+
+enum Constant {
+    
+    static let appGroup = "group.com.pun.rabin.iMessageApp"
+    
+    enum Database {
+        static let modelName = "iMessageDataModel"
+    }
+}
